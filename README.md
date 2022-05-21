1c312d09242e02aa3094eaabed4ccd39e2b1c35d
Zein Tonconi <zeintonconi@gmail.com>
2022-05-21 08:17:23 -0400

    Introduccion DHTML

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ejercicios-DHTML
---
+++ problema 1/functions.js
@@ -0,0 +1,9 @@
+function cambiarChiti() {
+    document.getElementById("parrafo").style.fontSize="10px";
+}
+function cambiarMediana() {
+    document.getElementById("parrafo").style.fontSize="13px";
+}
+function cambiarGrande () {
+    document.getElementById("parrafo").style.fontSize="20px";
+}
